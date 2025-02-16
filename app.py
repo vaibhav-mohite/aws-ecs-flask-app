05d1c0aae572c7b8f7d4216d10fbbf30596e840e
Vaibhav_Mohite _ <mohitevaibhav404@gmail.com>
2025-02-16 10:59:20 +0530

    Modified App.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Dosto, welcome to DevOps Zero To Hero (Junoon  Batch 9)'
+    return 'This App is deployed on AWS ECS'
 
 @app.route('/health')
 def health():
